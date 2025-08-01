428a66b4f1ed4a79b8fe7bfea52ff5ea55e7c8e2
git <f.pashaee@yahoo.com>
2025-07-31 20:44:38 -0400

    Initial Commit

---
+++ src/main/java/com/csl/kafkador/KafkadorApplication.java
@@ -0,0 +1,13 @@
+package com.csl.kafkador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkadorApplication.class, args);
+	}
+
+}
