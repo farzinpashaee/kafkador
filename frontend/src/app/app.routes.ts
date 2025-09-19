cfd894c48c595467d8fdd1cabcef0e320d6b5a0b
git <f.pashaee@yahoo.com>
2025-09-19 18:53:03 -0400

    ADD access-control component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { ConsumersComponent } from './components/consumers/consumers.component'
 import { ConnectComponent } from './components/connect/connect.component'
 import { BrokerComponent } from './components/broker/broker.component'
 import { TopicsComponent } from './components/topics/topics.component'
+import { AccessControlComponent } from './components/access-control/access-control.component'
 import { ConnectionsComponent } from './components/connections/connections.component'
 
 export const routes: Routes = [
@@ -21,6 +22,7 @@ export const routes: Routes = [
       { path: 'topics', component: TopicsComponent, title: 'Topics', data: { breadcrumb: 'Topics' } },
       { path: 'topic/:name', component: TopicComponent, title: 'Topic', data: { breadcrumb: 'Topics > {{Topic Name}}' } },
       { path: 'consumers', component: ConsumersComponent, title: 'Consumers', data: { breadcrumb: 'Consumers' } },
+      { path: 'access-control', component: AccessControlComponent, title: 'Access Control', data: { breadcrumb: 'Access Control' } },
       { path: 'connections', component: ConnectionsComponent, title: 'Connections', data: { breadcrumb: 'Connections' } }
     ]
   },
---
+++ frontend/src/app/components/access-control/access-control.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-access-control',
+  imports: [CommonModule],
+  templateUrl: './access-control.component.html',
+  styleUrl: './access-control.component.scss'
+})
+export class AccessControlComponent {
+
+}
