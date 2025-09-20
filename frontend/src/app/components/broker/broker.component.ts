c0143264c7a42670c570111bda925830155e16db
git <f.pashaee@yahoo.com>
2025-09-20 14:39:21 -0400

    ADD Search for broker config

--- frontend/src/app/components/broker/broker.component.ts
+++ frontend/src/app/components/broker/broker.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, PipeTransform  } from '@angular/core';
 import { ActivatedRoute,RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule, AsyncPipe, DecimalPipe  } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { startWith, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { ApiService } from '../../services/api.service';
 import { Broker } from '../../models/broker';
+import { Config } from '../../models/config';
 import { GenericResponse } from '../../models/generic-response';
 
 @Component({
   selector: 'app-broker',
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,ReactiveFormsModule],
   templateUrl: './broker.component.html',
   styleUrl: './broker.component.scss'
 })
@@ -15,7 +19,9 @@ export class BrokerComponent {
 
   brokerId!: string;
   broker!: Broker;
+  brokerConfig!: Config[];
   isLoading: boolean = true;
+  filter = new FormControl('', { nonNullable: true });
 
   constructor(private apiService: ApiService,
     private route: ActivatedRoute) {}
@@ -24,8 +30,21 @@ export class BrokerComponent {
     this.brokerId = this.route.snapshot.paramMap.get('id')!;
     this.apiService.getBrokerDetails(this.brokerId).subscribe((res: GenericResponse<Broker>) => {
       this.broker = res.data;
+      this.brokerConfig = res.data.config;
       this.isLoading = false;
+      this.filter.valueChanges.pipe( startWith(''),map((text: string) => this.search(text)))
+        .subscribe(filtered => {
+          this.brokerConfig = filtered;
+        });
     });
+
+  }
+
+  search(text: string): Config[] {
+     return this.broker.config.filter((config: Config) => {
+    		const term = text.toLowerCase();
+    		return ( config.name.toLowerCase().includes(term) );
+    	});
   }
 
 }
