1ae6647d25ad151d4ab954ec25720be9346bc425
git <f.pashaee@yahoo.com>
2025-10-02 18:25:50 -0400

    Update metric chart style

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { LogInterceptor } from './interceptors/log-interceptor';
@@ -11,6 +12,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideNoopAnimations(),
     provideHttpClient(
           withInterceptors([
             LogInterceptor,
--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,46 @@
 import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxChartsModule, Color, ScaleType } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule],
+  imports: [CommonModule,NgxChartsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  data = [
+    { name: 'a1', value: 5000 },
+    { name: 'a2', value: 3000 },
+    { name: 'a3', value: 2000 },
+    { name: 'a4', value: 3450 },
+    { name: 'a5', value: 2400 }
+  ];
 
+   data2 = [
+      { name: 'a1', value: 100 },
+      { name: 'a2', value: 5200 },
+      { name: 'a3', value: 1000 },
+      { name: 'a4', value: 1250 },
+      { name: 'a5', value: 2400 }
+    ];
+
+
+  data3 = [
+    { name: 'a1', value: 500 },
+    { name: 'a2', value: 2000 },
+    { name: 'a3', value: 2000 },
+    { name: 'a4', value: 200 },
+    { name: 'a5', value: 2400 }
+  ];
+
+
+  view = [50, 50];
+
+  customColorScheme: Color = {
+    name: 'whiteScheme',
+    selectable: true,
+    group: ScaleType.Ordinal,
+    domain: ['#FFF']
+  };
 }
