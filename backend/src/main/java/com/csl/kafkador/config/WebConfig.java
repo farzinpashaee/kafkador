4a07770c571d43a21d783aadfc73b9305a4290c0
git <f.pashaee@yahoo.com>
2025-11-25 20:17:27 -0500

    Update new cluster form

--- backend/src/main/java/com/csl/kafkador/config/WebConfig.java
+++ backend/src/main/java/com/csl/kafkador/config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor( new ResourceInterceptor(applicationConfig.getUrl()) )
                 .excludePathPatterns("/assets/**","/css/**","/js/**");
         registry.addInterceptor( new SessionInterceptor() )
-                .excludePathPatterns("/assets/**","/css/**","/js/**", "/error",
+                .excludePathPatterns("/assets/**","/css/**","/js/**", "/error",  "/api/disconnect", "/api/disconnect/**",
                         "/connect","/api/connect","/api/connect/**","/api/apm/**",
                         "/api/connection","/api/connection/**");
         registry.addInterceptor(localeChangeInterceptor());
